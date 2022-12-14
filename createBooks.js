e143867d69cadc5a82ce9b5063a530c76d3482dc
Ash Uppal <ashuppal@gmail.com>
2022-12-13 22:44:15 -0800

    added post and delete

--- createBooks.js
+++ createBooks.js
@@ -2,21 +2,75 @@
 
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Books = require('./models/books.js');
+const Books = require('./models/book.js');
 const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose.connect(MONGODB_URL);
 
-Books.create({
-  title: 'The Alchemist',
+async function seedDataBase() {
+  await Books.create({
+    title: 'Harry potter and the sorcerers stone',
+    description: 'This is a story about a wizard',
+    status: 'read',
+  })
 
-  description: "The Alchemist by Paulo Coelho continues to change the lives of its readers forever. With more than two million copies sold around the world, The Alchemist has established itself as a modern classic, universally admired.Paulo Coelho's masterpiece tells the magical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure as extravagant as any ever found. The story of the treasures Santiago finds along the way teaches us, as only a few stories can, about the essential wisdom of listening to our hearts, learning to read the omens strewn along life's path, and, above all, following our dreams. ",
-  
-  status: "Fiction"
-})
-.then(result => {
-  console.log('Heres our book!', result);
-})
-.catch(err => {
-  console.log('oh no, ', err);
-});
+  await Books.create({
+    title: 'Curious George',
+    description: 'A story about a monkey',
+    status: 'read',
+  })
+
+  await Books.create({
+    title: 'The Hobbit',
+    description: 'A story about a hobbit',
+    status: 'not read',
+  })
+  console.log('books added');
+
+  mongoose.disconnect();
+}
+
+seedDataBase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict'
+
+// require('dotenv').config()
+// const mongoose = require('mongoose')
+// const Books = require('./models/books.js')
+// const MONGODB_URL = process.env.MONGODB_URL
+
+// mongoose.connect(MONGODB_URL)
+
+// async function createBooks() {
+//   await Books.create({
+//     title: 'test',
+
+//     description: 'This is a test ',
+
+//     status: 'Fiction'
+//   })
+//   mongoose.disconnect();
+// }
+//  createBooks();
+
+
+
--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const books = require('./models/books');
-const { default: mongoose } = require('mongoose');
-const { response } = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const books = require('./models/books')
+const { default: mongoose } = require('mongoose')
+const { response } = require('express')
+const app = express()
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL
 
-mongoose.connect(MONGODB_URL);
-app.use(cors());
-app.use(express.json());
+mongoose.connect(MONGODB_URL)
+app.use(cors())
+app.use(express.json())
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.get('/books', async (request, response) => {
+  try {
+    let title = request.query.title
 
-let title = request.query.title;
-
-  let result = [];
-  if (title){
+  let result = []
+  if (title) {
     result = await books.find({
       title: title
     })
-  }else {
-
-    result = await books.find();
+  } else {
+    result = await books.find()
   }
 
-  response.send(result);
-
+  response.send(result).status(200)
+  } catch (error) {
+    response.send(error).status(500)
+  }
+  
 })
 
-app.post('/books', async(request, response)=>{
-  let newBook = await books.create(request.body);
-  response.send(newBook);
+app.post('/books', async (request, response) => {
+  let newBook = await books.create(request.body)
+  response.send(newBook)
 })
 
-app.delete('/books/:id', async(request, response)=>{
+app.delete('/books/:id', async (request, response) => {
+  let id = request.params.id
 
-  let id = request.params.id;
+  let deletedBook = await books.findByIdAndDelete(id)
 
-  let deletedBook = await books.findByIdAndDelete(id);
-
-  response.send(deletedBook);
+  response.send(deletedBook)
 })
 
-app.use;
+app.use
 app.use('*', (request, response) => {
-  response.status(500).send('Invalid Request, page not found.');
-});
+  response.status(500).send('Invalid Request, page not found.')
+})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
